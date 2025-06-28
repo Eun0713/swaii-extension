e1986c6cb5ede5b016bc4ab5cbf8e60943b59950
Eun0713 <daeunlee0713@naver.com>
2025-06-28 14:54:34 +0900

    chore: Tailwind CSS 설치 및 초기 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <button className="bg-blue-500 px-4 py-2 text-white">버튼</button>
   </StrictMode>
 );
