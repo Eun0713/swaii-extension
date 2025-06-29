97230b6d3ddeed70e908bf4c78adc121c452fa98
Eun0713 <daeunlee0713@naver.com>
2025-06-29 15:17:56 +0900

    feat: 시작하기 버튼에 페이지 이동 기능 연결

--- src/pages/EntryPoint.jsx
+++ src/pages/EntryPoint.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 import GestureCircleCursor from "@/assets/circle-gesture.svg";
 import LogoSwaii from "@/assets/logo-swaii.svg?react";
 import { gestureItems } from "@/constants/gestureItems";
 
 const EntryPoint = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="relative h-[480px] w-[640px] overflow-hidden bg-slate-900 p-6 text-white">
       <img
@@ -31,7 +35,10 @@ const EntryPoint = () => {
         ))}
       </ul>
 
-      <button className="absolute bottom-5 right-5 rounded-md bg-zinc-600 px-4 py-2 text-lg font-semibold text-white">
+      <button
+        className="absolute bottom-5 right-5 rounded-md bg-zinc-600 px-4 py-2 text-lg font-semibold text-white"
+        onClick={() => navigate("/settings")}
+      >
         시작하기
       </button>
     </div>
