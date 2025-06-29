0939f908cb0720c534805ad5ec355b71abf95e84
Eun0713 <daeunlee0713@naver.com>
2025-06-30 00:51:43 +0900

    feat: EditPattern에 초기 데이터(mock) 설정 추가

--- src/pages/EditPattern.jsx
+++ src/pages/EditPattern.jsx
@@ -15,7 +15,14 @@ const EditPattern = () => {
       title="패턴 수정"
       description={`현재 설정된 마우스 제스처에 연결된 사이트, 패턴 모양, 실행 동작을 \n확인하고 필요에 따라 자유롭게 수정할 수 있습니다.`}
     >
-      <PatternForm onCancel={handleCancel} />
+      <PatternForm
+        initialData={{
+          site: "Notion.com",
+          gesture: "무한대 패턴",
+          action: "새 페이지 생성",
+        }}
+        onCancel={handleCancel}
+      />
     </PatternLayout>
   );
 };
