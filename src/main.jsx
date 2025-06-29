8a01695b1dc92538f7f8b0915764b92bd062fa11
Eun0713 <daeunlee0713@naver.com>
2025-06-29 15:05:58 +0900

    refactor: Router 경로 alias 적용 및 글로벌 스타일 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-import Router from "./Router";
+import Router from "@/Router";
+import "@/index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
