bea7cf5def28f55c178e07ec5f9edc021953c599
Eun0713 <daeunlee0713@naver.com>
2025-06-29 15:13:28 +0900

    refactor: gestureItems 배열 생성 및 EntryPoint에서 map 렌더링 적용

---
+++ src/constants/gestureItems.js
@@ -0,0 +1,24 @@
+import IconGoogle from "@/assets/icon-google.svg";
+import IconNotion from "@/assets/icon-notion.svg";
+import IconYoutube from "@/assets/icon-youtube.svg";
+
+export const gestureItems = [
+  {
+    icon: IconYoutube,
+    site: "Youtube",
+    gesture: "원형 그리기",
+    action: "볼륨 조절",
+  },
+  {
+    icon: IconNotion,
+    site: "Notion",
+    gesture: "원형 그리기",
+    action: "새 페이지 추가",
+  },
+  {
+    icon: IconGoogle,
+    site: "Google",
+    gesture: "Z자 그리기",
+    action: "새 탭 열기",
+  },
+];
--- src/pages/EntryPoint.jsx
+++ src/pages/EntryPoint.jsx
@@ -1,8 +1,6 @@
 import GestureCircleCursor from "@/assets/circle-gesture.svg";
-import IconGoogle from "@/assets/icon-google.svg";
-import IconNotion from "@/assets/icon-notion.svg";
-import IconYoutube from "@/assets/icon-youtube.svg";
 import LogoSwaii from "@/assets/logo-swaii.svg?react";
+import { gestureItems } from "@/constants/gestureItems";
 
 const EntryPoint = () => {
   return (
@@ -23,24 +21,14 @@ const EntryPoint = () => {
       </p>
 
       <ul className="mt-9 space-y-4 text-lg">
-        <li className="flex items-center">
-          <img src={IconYoutube} className="mr-3" />
-          <span>
-            Youtube에서 <b>“원형 그리기”</b> → 볼륨 조절
-          </span>
-        </li>
-        <li className="flex items-center">
-          <img src={IconNotion} className="mr-3" />
-          <span>
-            Notion에서 <b>“원형 그리기”</b> → 새 페이지 추가
-          </span>
-        </li>
-        <li className="flex items-center">
-          <img src={IconGoogle} className="mr-3" />
-          <span>
-            Google에서 <b>“Z자 그리기”</b> → 새 탭 열기
-          </span>
-        </li>
+        {gestureItems.map(({ icon, site, gesture, action }) => (
+          <li key={site} className="flex items-center">
+            <img src={icon} className="mr-3" />
+            <span>
+              {site}에서 <b>"{gesture}"</b> → {action}
+            </span>
+          </li>
+        ))}
       </ul>
 
       <button className="absolute bottom-5 right-5 rounded-md bg-zinc-600 px-4 py-2 text-lg font-semibold text-white">
