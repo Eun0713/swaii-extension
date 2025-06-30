83d6af6459f92ac0d4c7d19e12e6541d5de6fa13
Eun0713 <daeunlee0713@naver.com>
2025-06-30 23:01:28 +0900

    refactor: Router 대신 App 컴포넌트 렌더링하도록 main.jsx 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-import Router from "@/Router";
+import App from "@/App";
 import "@/index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
-    <Router />
+    <App />
   </HashRouter>
 );
