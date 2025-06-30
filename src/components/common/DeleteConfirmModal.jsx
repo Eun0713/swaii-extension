1f5952165e0eeb2beb0e6d7d3656b01aaa04ca42
Eun0713 <daeunlee0713@naver.com>
2025-06-30 20:46:38 +0900

    style: 삭제 확인 모달 크기 및 여백(padding) 조정

--- src/components/common/DeleteConfirmModal.jsx
+++ src/components/common/DeleteConfirmModal.jsx
@@ -4,7 +4,7 @@ import Button from "@/components/common/Button";
 const DeleteConfirmModal = ({ title, description, onCancel, onConfirm }) => {
   return (
     <div className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
-      <div className="w-[360px] rounded-xl bg-white p-6">
+      <div className="w-[350px] rounded-xl bg-white p-4">
         <div className="mb-2 flex justify-center">
           <WarningIcon className="h-10 w-10" />
         </div>
