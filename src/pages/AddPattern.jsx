376da213ccd26afcebdb9f4cdc7adb7686b7b0b8
Eun0713 <daeunlee0713@naver.com>
2025-07-11 15:30:10 +0900

    refactor: JSX 내 불필요한 Fragment 정리

--- src/pages/AddPattern.jsx
+++ src/pages/AddPattern.jsx
@@ -49,20 +49,18 @@ const AddPattern = () => {
   };
 
   return (
-    <>
-      <HeaderLayout
-        title="새 마우스 패턴 추가"
-        description={`원하는 사이트에 마우스 제스처와 실행할 동작을 연결해\n새로운 패턴을 추가할 수 있습니다.`}
-        onClose={handleClose}
-      >
-        <AlertMessage {...alert} />
-        <PatternForm
-          onCancel={handleCancel}
-          onSubmit={handleSubmit}
-          showCustomPatternButton
-        />
-      </HeaderLayout>
-    </>
+    <HeaderLayout
+      title="새 마우스 패턴 추가"
+      description={`원하는 사이트에 마우스 제스처와 실행할 동작을 연결해\n새로운 패턴을 추가할 수 있습니다.`}
+      onClose={handleClose}
+    >
+      <AlertMessage {...alert} />
+      <PatternForm
+        onCancel={handleCancel}
+        onSubmit={handleSubmit}
+        showCustomPatternButton
+      />
+    </HeaderLayout>
   );
 };
 
