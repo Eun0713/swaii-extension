399caa5de5d3b2f89cee3f3314e977d711ce14a9
Eun0713 <daeunlee0713@naver.com>
2025-08-20 17:39:49 +0900

    refactor: app 폴더 타입스크립트로 마이그레이션

--- src/app/initializeUserSession.ts
+++ src/app/initializeUserSession.ts
@@ -1,34 +1,42 @@
 import { getUserGestures } from "@/services/gestureService";
 import { getUserMappings } from "@/services/mappingService";
+import { Gesture } from "@/types/gesture";
+import { Mapping } from "@/types/mapping";
 import { requestGoogleLogin } from "@/utils/auth/requestGoogleLogin";
 import createUserGestureStore from "@/utils/gesture/userGestureStorage";
 import userMappingStorage from "@/utils/mapping/userMappingStorage";
 
-export const initializeUserSession = async () => {
+interface GoogleUserInfo {
+  email: string;
+}
+
+export const initializeUserSession = async (): Promise<{
+  userEmail: string;
+}> => {
   const token = await requestGoogleLogin();
 
   const res = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
     headers: { Authorization: `Bearer ${token}` },
   });
 
-  const userInfo = await res.json();
-  const userEmail = userInfo.email;
+  const userInfo: GoogleUserInfo = await res.json();
+  const userEmail: string = userInfo.email;
 
-  const gestureList = await getUserGestures(userEmail);
+  const gestureList: Gesture[] = await getUserGestures(userEmail);
   const userGestureStore = createUserGestureStore(userEmail);
 
   for (const gesture of gestureList) {
     await userGestureStore.save(gesture);
   }
 
-  const mappingList = await getUserMappings(userEmail);
+  const mappingList: Mapping[] = await getUserMappings(userEmail);
   const userMappingStore = userMappingStorage(userEmail);
 
   for (const mapping of mappingList) {
     await userMappingStore.save(mapping);
   }
 
-  chrome.storage.local.set({ userEmail: userEmail });
+  await chrome.storage.local.set({ userEmail });
 
   return { userEmail };
 };
